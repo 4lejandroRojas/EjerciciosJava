b74f8cf8ea13b70f785912e514bfedb0524527bb
4lejandroRojas <4lejandroRojas@gmail.com>
2018-11-12 22:04:05 -0600

    Se agrega clase _03_MultipleChoice

---
+++ src/_03_MultipleChoice.java
@@ -0,0 +1,15 @@
+
+public class _03_MultipleChoice {
+
+	static void TestFirstApp(int x, int y, int m){
+		if(x == 5) m=y;
+		else m=x;
+	}
+	public static void main(String[] args) {
+		int i=6, j=4, k=9;
+		_03_MultipleChoice.TestFirstApp(i, j, k);
+		System.out.println(k);
+
+	}
+
+}
